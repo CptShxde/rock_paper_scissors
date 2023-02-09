3883df0be64ec15750e7d46aae1cb20783c051ed
CptShxde <ervinheldic04@gmail.com>
2023-02-09 08:43:19 +0100

    five rounds added

--- game.js
+++ game.js
@@ -1,75 +1,50 @@
-const choices = ["rock", "paper", "scissors"];
-
-function getComputerChoice() {
-    return choices[Math.floor(Math.random()* choices.length)]
-}
-
-let playerSelection = prompt("type: rock, paper or scissors.").toLowerCase();
-let computerSelection = getComputerChoice();
-
-/* function playGame(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return ("It's a draw!")
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return ("You Win! Rock beats Scissors.");
-    }
-    else if (playerSelection === "rock" && computerSelection === "paper") {
-        return ("You Lose! Paper beats Rock.");
-    }
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        return ("You Win! Paper beats Rock.");
-    }
-    else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return ("You Lose! Scissors beats Papers.");
-    }
-    else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return ("You Win! Scissors beats Paper.");
-    }
-    else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return ("You Lose! Rock beats Scissors.");
-    }
-    else {
-        return ("Something went wrong...");
-    }
-
-} */
-
-
-/* make a 5 round function game 
-loop the playGame function to play 5 times 
-console log each round */
-
 function game() {
     for (let i = 0; i < 5; i++) {
+        const choices = ["rock", "paper", "scissors"];
+
+        function getComputerChoice() {
+            return choices[Math.floor(Math.random()* choices.length)]
+        }
+
+        let playerSelection = prompt("type: rock, paper or scissors.").toLowerCase();
+        let computerSelection = getComputerChoice();
+
         function playGame(playerSelection, computerSelection) {
+            let score = 0
+
             if (playerSelection == computerSelection) {
-                return ("It's a draw!")
+                return ("It's a draw! Your score is " + score);
             }
             else if (playerSelection === "rock" && computerSelection === "scissors") {
-                return ("You Win! Rock beats Scissors.");
+                let score = score + 1;
+                return ("You Win! Rock beats Scissors. Your score is " + score);
             }
             else if (playerSelection === "rock" && computerSelection === "paper") {
-                return ("You Lose! Paper beats Rock.");
+                return ("You Lose! Paper beats Rock. Your score is " + score);
             }
             else if (playerSelection === "paper" && computerSelection === "rock") {
-                return ("You Win! Paper beats Rock.");
+                let score = score + 1;
+                return ("You Win! Paper beats Rock. Your score is " + score);
             }
             else if (playerSelection === "paper" && computerSelection === "scissors") {
-                return ("You Lose! Scissors beats Papers.");
+                return ("You Lose! Scissors beats Papers. Your score is " + score);
             }
             else if (playerSelection === "scissors" && computerSelection === "paper") {
-                return ("You Win! Scissors beats Paper.");
+                let score = score + 1;
+                return ("You Win! Scissors beats Paper. Your score is " + score);
             }
             else if (playerSelection === "scissors" && computerSelection === "rock") {
-                return ("You Lose! Rock beats Scissors.");
+                return ("You Lose! Rock beats Scissors. Your score is " + score);
             }
             else {
                 return ("Something went wrong...");
             }
-            
+
         }
+        let result = playGame(playerSelection, computerSelection);
+        console.log(result);
     }
-    let result = playGame(playerSelection, computerSelection);
-    console.log(result);
 }
+
+    
+
