c42c37082a2cdafa75daf1186e97833417b01d7d
CptShxde <ervinheldic04@gmail.com>
2023-02-16 11:36:31 +0100

    cant increment score

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const arrOfChoices = ["rock", "paper", "scissors"];
@@ -13,27 +15,44 @@ function playRound(playerSelection, computerSelection) {
         return "Tie";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         return "Player wins";
+        ++playerScore;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         return "Player wins";
+        ++playerScore;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         return "Player wins";
+        ++playerScore;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         return "Computer wins";
+        ++computerScore;
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         return "Computer wins";
+        ++computerScore;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         return "Computer wins";
+        ++computerScore;    
     }
 }
 
+function roundScore(playerScore, computerScore) {
+    if (playRound === "Player wins") {
+        playerScore++;
+    }
+    else if (playRound === "Computer wins") {
+        computerScore++;
+    }
+}
 
 
 function game() {
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt("Type Rock, Paper or Scissors: ", "Rock, Paper, Scissors");
         const computerSelection = getComputerChoice();
-        playRound(computerSelection, playerSelection);   
+        console.log(playRound(computerSelection, playerSelection)); 
+        roundScore(); 
+        console.log(playerScore);
+        console.log(computerScore);
     }
 }
 
-console.log(game());
+game();
